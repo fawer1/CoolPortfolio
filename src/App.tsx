792eb2c0abd2c2ad75bebfeeb0390e014a3ad293
Furkan Smail <furkanmusa2@gmail.com>
2026-01-15 18:08:24 +0100

    deleted the unnecessary main.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 import ScrollSection from './components/ScrollSection';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
+
 
 export default function App() {
 
